e14f3599ba94b62dc57ca4ed9e19bbd620013d59
Ninh Bui <ninh.bui@stud.h-da.de>
2021-05-02 11:57:13 +0200

    double, identifier recognise

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -38,7 +38,7 @@ func (lexer *Lexer) Next() Token {
 			if err == io.EOF {
 				return Token{Type: EOF}
 			}
-			return Token{}
+			return Token{Type: ILLEGAL}
 		}
 
 		lexer.position.Column++
@@ -78,6 +78,8 @@ func (lexer *Lexer) nextNumber() Token {
 	// TODO: return token with type INTEGER and string value if it's a Integer. Or Double if it's a double
 
 	var number string
+	var dotCounter = true
+
 	for {
 		r, _, err := lexer.reader.ReadRune()
 		if err != nil {
@@ -90,18 +92,31 @@ func (lexer *Lexer) nextNumber() Token {
 		lexer.position.Column++
 		if unicode.IsDigit(r) {
 			number = number + string(r)
+		} else if (string(r) == "." && dotCounter) {
+			//Double only dot
+			number = number + string(r)
+			dotCounter = false
 		} else {
 			// scanned something not in the identifier
 			lexer.back()
-			return Token{Type: IDENTIFIER, Position: lexer.position, Text: number}
+			//Double recognised
+			if !dotCounter {
+				return Token{Type: DOUBLE, Position: lexer.position, Text: number}
+			}
+			return Token{Type: INTEGER, Position: lexer.position, Text: number}
 		}
 	}
 
+	if !dotCounter {
+		return Token{Type: DOUBLE}
+	}
 	return Token{Type: INTEGER}
 }
 
 func (lexer *Lexer) nextIdentifier() Token {
 	var identifier string
+	//var firstChar = true
+
 	for {
 		r, _, err := lexer.reader.ReadRune()
 		if err != nil {
@@ -114,8 +129,11 @@ func (lexer *Lexer) nextIdentifier() Token {
 		lexer.position.Column++
 		if unicode.IsLetter(r) {
 			identifier = identifier + string(r)
+		} else if unicode.IsDigit(r) {
+			identifier = identifier + string(r)
 		} else {
 			// scanned something not in the identifier
+			// TODO: lexer must read number after first char to identifier
 			lexer.back()
 			return Token{Type: IDENTIFIER, Position: lexer.position, Text: identifier}
 		}
